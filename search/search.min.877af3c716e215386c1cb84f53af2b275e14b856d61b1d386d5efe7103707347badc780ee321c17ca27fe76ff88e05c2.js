7f1b8c1f276ea82e73357c5e86357cfa030278cc
Jason Yao <Hello@JasonYao.com>
2021-03-07 19:44:11 +0000

    [Build] d6ac304

---
+++ search/search.min.877af3c716e215386c1cb84f53af2b275e14b856d61b1d386d5efe7103707347badc780ee321c17ca27fe76ff88e05c2.js
@@ -0,0 +1,35 @@
+var idx,searchInput,searchResults=null,documents={};function parseISOString(b){var a=b.split(/\D+/);return new Date(Date.UTC(a[0],--a[1],a[2],a[3],a[4],a[5],a[6]))}function kebab(a){const b=a.replace(/\s+/g,'-').toLowerCase();return b}function createSearchResult(a){const b=document.createElement('article');b.classList.add('search-result');const c={weekday:'short',year:'numeric',month:'short',day:'numeric'},d=parseISOString(a.publish_date),e=d.toLocaleDateString("en-US",c);b.innerHTML=`<div class="post-thumbnail">
+      <a href="${a.permalink}">
+          <img width="150" src="${a.thumbnail}" alt="${a.thumbnail_alt_text}" loading="lazy">
+      </a>
+  </div>
+
+  <div class="post-title">
+      <h3><a href="${a.permalink}">${a.title}</a>
+      </h3>
+  </div>
+
+  <div class="post-content">
+      <div class="p_part"><p>${a.summary}</p></div>
+  </div>
+
+  <div class="post-footer">
+      <div class="meta">
+          <div class="info">
+              <!-- Displays the publish date instead of date when post was started -->
+              <em class="fas fa-calendar-day"></em>
+              <time datetime="${a.publish_date}">
+              ${e}
+              </time>
+
+              <em class="fas fa-stopwatch"></em>
+              <span class="reading-time">${a.reading_time}</span>
+
+              <em class="fas fa-list-alt"></em>
+              <span class="separator category-wrapper"></span>
+
+              <em class="fas fa-tags"></em>
+              <span class="separator tag-wrapper"></span>
+          </div>
+      </div>
+  </div>`;const f=a.categories,g=b.getElementsByClassName('category-wrapper')[0];f.forEach(b=>{const c=kebab(b),a=document.createElement('a'),d=document.createTextNode(b);a.appendChild(d),a.classList.add('category'),a.href=`/categories/${c}/`,g.appendChild(a)});const h=a.tags,i=b.getElementsByClassName('tag-wrapper')[0];return h.forEach(b=>{const c=kebab(b),a=document.createElement('a'),d=document.createTextNode(b);a.appendChild(d),a.classList.add('tag'),a.href=`/tags/${c}/`,i.appendChild(a)}),b}function renderSearchResults(a){a.length>0?(a.length>9&&(a=a.slice(0,10)),searchResults.innerHTML='',a.forEach(a=>{const b=documents[a.ref],c=createSearchResult(b);searchResults.appendChild(c)})):searchResults.innerHTML='<p>No results found.</p>'}function registerSearchHandler(){searchInput.oninput=function(c){var a,b;searchInput.value==''?searchResults.innerHTML='':(a=c.target.value,b=idx.search(a+'*'),renderSearchResults(b))},searchInput.focus(),searchInput.placeholder=''}window.onload=function(){searchInput=document.getElementById('search-input'),searchResults=document.getElementById('search-results'),fetch('/posts/index.json',{method:'get'}).then(a=>a.json()).then(a=>{idx=lunr(function(){this.ref('permalink'),this.field('title'),this.field('categories'),this.field('tags'),this.field('content'),this.field('summary'),{posts}=a,posts.forEach(function(a){this.add({permalink:a.metadata.permalink,title:a.metadata.title,categories:a.metadata.categories,tags:a.metadata.tags,content:a.data.content,thumbnail:a.metadata.thumbnail,summary:a.metadata.summary}),documents[a.metadata.permalink]={permalink:a.metadata.permalink,title:a.metadata.title,categories:a.metadata.categories,tags:a.metadata.tags,content:a.data.content,thumbnail:a.metadata.thumbnail,thumbnail_alt_text:a.metadata.thumbnail_alt_text,summary:a.metadata.summary,publish_date:a.metadata.published_on,reading_time:a.metadata.reading_time}},this)}),registerSearchHandler()}).catch(a=>{searchResults.innerHTML=`<p>${a}</p>`})}
